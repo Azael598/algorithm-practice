ff4f87615d4545dfbcba1cfc0c37c6520a61ec62
Azael598 <987819317@qq.com>
2020-08-17 14:44:42 +0800

    quickSort and mergeSort

---
+++ src/sort/mergeSort.java
@@ -0,0 +1,34 @@
+package sort;
+
+public class mergeSort {
+    public void merge(int[] A, int L1, int R1, int L2, int R2) {
+        int i = A[L1], j = A[L2];
+        int[] temp = new int[100];
+        int index = 0;
+        while (i <= R1 && j <= R2) {
+            if (A[i] <= A[j]) {
+                temp[index++] = A[i++];
+            } else {
+                temp[index++] = A[j++];
+            }
+        }
+        while (i <= R1) {
+            temp[index++] = A[i++];
+        }
+        while (j <= R2) {
+            temp[index++] = A[j++];
+        }
+        for (i = 0; i < index; i++) {
+            A[L1 + i] = temp[i];
+        }
+    }
+
+    void mergeSort(int[] A, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(A, left, mid);
+            mergeSort(A, mid + 1, right);
+            merge(A, left, mid, mid + 1, right);
+        }
+    }
+}
