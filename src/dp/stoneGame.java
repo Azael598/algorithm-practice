98f8ecb215ea80f8d9df0225a51df12b2aab0fdb
Azael598 <987819317@qq.com>
2020-09-21 20:13:08 +0800

    stoneGame

---
+++ src/dp/stoneGame.java
@@ -0,0 +1,52 @@
+package dp;
+
+public class stoneGame {
+    class Pair {
+        public int fir;
+        public int sec;
+
+        public Pair(int fir, int sec) {
+            this.fir = fir;
+            this.sec = sec;
+        }
+    }
+
+    boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        Pair[][] dp = new Pair[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = new Pair(0, 0);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            dp[i][i].fir = piles[i];
+            dp[i][i].sec = 0;
+        }
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i <= n - l; i++) {
+                int j = l + i - 1;
+                int left = piles[i] + dp[i + 1][j].sec;
+                int right = piles[j] + dp[i][j - 1].sec;
+                if (left > right) {
+                    dp[i][j].fir = left;
+                    dp[i][j].sec = dp[i + 1][j].fir;
+                } else {
+                    dp[i][j].fir = right;
+                    dp[i][j].sec = dp[i][j - 1].fir;
+                }
+            }
+        }
+        Pair res = dp[0][n - 1];
+        if(res.fir>0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int []piles=new int[]{5,3,4,5};
+        System.out.println(new stoneGame().stoneGame(piles));
+    }
+}
